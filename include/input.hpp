da953e9a30d4f4714f9c888d6fa2d1c610ad8914
Megraj Chauhan <megrajchauhan123@gmail.com>
2024-10-05 10:43:04 +0545

    Mistakes

---
+++ include/input.hpp
@@ -0,0 +1,42 @@
+#ifndef _INPUT_
+#define _INPUT_
+
+// Read the source file and make one pass to resolve includes
+#include <filesystem>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <unordered_set>
+#include "log.hpp"
+
+struct Details
+{
+    size_t lstart, lend;
+};
+
+struct File
+{
+    std::string _file_contents;
+    Details fdetails;
+    std::string fpath;
+    std::vector<File> next_files;
+    File *parent;
+};
+
+class Input
+{
+    File entry;
+    std::unordered_set<std::string> included_files; // make sure some files are not included multiple times
+public:
+    Input(std::string entry_file_path);
+
+    bool read_input();
+
+    bool first_pass(size_t _start);
+
+    constexpr File* get_result();
+
+    void back_trace_error_log();
+};
+
+#endif
---
+++ include/log.hpp
@@ -0,0 +1,8 @@
+#ifndef _LOG_
+#define _LOG_
+
+#include <iostream>
+
+#define log(pre, msg) std::cerr << pre<< ": "<< msg
+
+#endif
